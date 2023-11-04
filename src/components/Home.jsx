d4e502c7ed898086b3e6dd8a76cde2adb78ad176
sisi wang <sisiwang242@gmail.com>
2023-11-04 11:21:11 -0400

    update navbar file

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi';
+
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-[#e5ddf4]'>
+
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi';
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+import Logo from "../assets/logo1.png";
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-8 bg-[#cbb9f7] text-black'>
+        <div>
+            <img src={Logo} alt="Logo Image" style={{width: '62px'}} />
+        </div>
+
+        {/* menu */}
+        <div className='hidden md:flex'>
+            <ul className='hidden md:flex text-xl gap-3'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        {/* Hamburger */}
+        <div onClick={handleClick} className='absolute top-6 right-6 md:hidden z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+
+        {/* Mobile menu */}
+        <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#cbb9f7] flex flex-col justify-center items-center'}>
+            <ul>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Work</li>
+                <li className='py-6 text-4xl'>Contact</li>
+            </ul>
+        </div>
+
+        {/* social icons */}
+        <div className='flex fixed flex-col top-[35%] left-0'>
+            <ul>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300'
+                    style={{background: 'linear-gradient(to right, white 60%, blue 40%'}} >
+                    <a className='flex justify-between items-center w-full text-gray-100' 
+                        href="/">
+                        <span className='text-black'>LinkedIn</span>
+                        <FaLinkedin size={30} />
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
