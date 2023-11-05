e7ae2c64bad1fab7b768cfc089f6563d075d0fe6
sisi wang <sisiwang242@gmail.com>
2023-11-05 10:33:42 -0500

    update about file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
                 {/* <div></div> */}
             </div>
             <div className='max-w-[1000px] w-full grid grid-cols-1 sm:grid-cols-2'>
-                    <div className='px-4 pt-3 flex justify-center items-center hover:scale-125'>
+                    <div className='px-4 py-5 pt-4 pt-3 flex justify-center items-center hover:scale-125'>
                         <img src={myPic} className='w-60 h-60 rounded-full shadow-lg shadow-[#f3deea]' alt="My Picture" />
                     </div>
 
