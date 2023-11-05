f8e65248c31860347f78e7f5d3da42e982e1cc28
sisi wang <sisiwang242@gmail.com>
2023-11-05 10:25:52 -0500

    update work file

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import RecipeImg from '../assets/RecipeApp.png'
 
 const Work = () => {
+  const [mobile, setMobile] = useState(false);
   return (
     <div name='work' className='w-full md:h-screen bg-[#e5ddf4]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
@@ -11,8 +12,8 @@ const Work = () => {
         </div>
 
         {/* Project 1 */}
-        <div className='grid grid-cols-1 sm:grid-cols-2 gap-6 sm:justify-center sm:items-center shadow-lg shadow-[#cbb9f7] px-4' 
-          style={{ gridTemplateColumns: '2fr 1fr' }}>
+        <div className={`grid grid-cols-1 sm:grid-cols-2 gap-6 sm:justify-center sm:items-center shadow-lg shadow-[#cbb9f7] px-4 py-4 
+          ${!mobile ? 'grid-cols-1' : 'grid-cols-2'}`}>
         <div className='col-span-2 sm:col-span-1'>
           <div className='max-w-[1000px] mx-auto text-center'>
               <div style={{backgroundImage: `url(${RecipeImg})`}}
@@ -37,9 +38,9 @@ const Work = () => {
               </div>
             </div>
         </div>
-            <div className='flex flex-col text-center'>
-              <h1 className='text-2xl text-purple-800 py-4'>Recipe Application - React JS</h1>
-              <p className='text-[18px] py-4'>
+            <div className='sm:text-left flex flex-col' >
+              <h1 className='text-2xl text-purple-800 py-4 '>Recipe Application - React JS</h1>
+              <p className='text-[18px] py-4 text-left'>
                 Discovering your favorite recipes has never been easier with our React JS Recipe Application. With a simple and intuitive interface, you can search for recipes by entering the name of your desired dish or food. Just type it in and hit 'Enter' to let our app find the perfect recipes for you. Whether you're a culinary enthusiast or just looking for quick and delicious meal ideas, our Recipe Application is your go-to source for a world of culinary inspiration.
               </p>
             </div>
