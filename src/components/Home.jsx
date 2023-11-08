40b7d961472d7df49eb5795584137f6df02335b5
sisi wang <sisiwang242@gmail.com>
2023-11-07 21:18:27 -0500

    update btn

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
             </li>
             <li className='w-[50px] h-[50px] rounded-lg flex justify-center items-center bg-[#888888]
                text-white text-4xl shadow-md shadow-gray-400 hover:scale-110'>
-              <a href="/src/assets/Resume.pdf" download="Resume.pdf">
+              <a href="https://raw.githubusercontent.com/Sisi-tech/portfolio/main/src/assets/Resume.pdf" download="Resume.pdf">
                 <BsFillPersonLinesFill /> 
               </a>
             </li>
